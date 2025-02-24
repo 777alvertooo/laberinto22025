ff4556655f33a97a63d48ad888e3904f3e3369a6
Alberto <albertoparlaortiz@gmail.com>
2025-02-24 08:37:20 +0100

    decorator y strategy

--- juegoLaberinto.py
+++ juegoLaberinto.py
@@ -5,6 +5,49 @@ class ElementoMapa:
     def entrar(self):
         pass
 
+class Decorator(ElementoMapa):
+    def __init__(self, em):
+        super().__init__()
+        self.em = em
+
+
+class Bomba(Decorator):
+    def __init__(self, em):
+        super().__init__(em)
+        self.activa = False
+
+    def esBomba(self):
+        return True
+    
+
+class Bicho:
+    def __init__(self, vidas, poder, posicion, modo):
+        self.vidas = vidas
+        self.poder = poder
+        self.posicion = posicion
+        self.modo = modo
+
+    def iniAgresivo(self):
+        self.modo = Agresivo()
+        self.poder = 10
+        self.vidas = 5
+
+    def iniPerezoso(self):
+        self.poder = 1
+        self.vidas = 5
+
+class Modo:
+    def __init__(self):
+        pass
+
+class Agresivo(Modo):
+    def __init__(self):
+        super().__init__()
+
+class Perezoso(Modo):
+    def __init__(self):
+        super().__init__()
+
 class Habitacion(ElementoMapa):
     def __init__(self, num):
         super().__init__()
@@ -79,6 +122,10 @@ class Laberinto(ElementoMapa):
 class Juego:
     def __init__(self):
         self.laberinto = Laberinto()
+        self.bicho = []
+
+    def agregar_bicho(self, bicho):
+        self.bichos.append(bicho)
 
     def iniciar_juego(self):
         # Lógica para iniciar el juego
@@ -94,7 +141,73 @@ class Juego:
         laberinto.agregar_habitacion(habitacion1)
         laberinto.agregar_habitacion(habitacion2)
         return laberinto
+    
+
+    def crearLaberinto2HabBomba(self, creator):
+        laberinto = creator.crear_laberinto()
+        habitacion1 = creator.crear_habitacion(1)
+        habitacion2 = creator.crear_habitacion(2)
+        puerta = creator.crear_puerta(habitacion1, habitacion2)
+
+        habitacion1.sur = puerta
+        habitacion2.norte = puerta
+
+        pared1 = creator.crear_pared()
+        bomba1 = creator.crear_bomba(pared1)
+        habitacion1.este = bomba1
+
+        pared2 = creator.crear_pared()
+        bomba2 = creator.crear_bomba(pared2)
+        habitacion2.oeste = bomba2
+
+        laberinto.agregar_habitacion(habitacion1)
+        laberinto.agregar_habitacion(habitacion2)
+        return laberinto
 
     def obtenerHabitacion(self, num):
         return self.laberinto.obtenerHabitacion(num)
+    
+
+    def crearLaberinto4Hab(self, creator):
+        laberinto = creator.crear_laberinto()
+
+        hab1 = creator.crear_habitacion(1)
+        hab2 = creator.crear_habitacion(2)
+        hab3 = creator.crear_habitacion(3)
+        hab4 = creator.crear_habitacion(4)
+
+        puerta1 = creator.crear_puerta(hab1, hab2)
+        puerta2 = creator.crear_puerta(hab1, hab3)
+        puerta3 = creator.crear_puerta(hab2, hab4)
+        puerta4 = creator.crear_puerta(hab3, hab4)
+
+        hab1.sur = puerta1
+        hab1.este = puerta2
+        hab3.oeste = puerta2
+        hab3.sur = puerta4
+        hab2.norte = puerta1
+        hab2.este = puerta3
+        hab4.norte = puerta4
+        hab4.oeste = puerta3
+
+        bicho1 = creator.crear_bicho(5, 10, hab1, creator.crear_modo_agresivo())
+        self.agregar_bicho(bicho1)
+        bicho3 = creator.crear_bicho(5, 10, hab3, creator.crear_modo_agresivo())
+        self.agregar_bicho(bicho3)
+        bicho2 = creator.crear_bicho(5, 1, hab2, creator.crear_modo_perezoso())
+        self.agregar_bicho(bicho2)
+        bicho4 = creator.crear_bicho(5, 1, hab4, creator.crear_modo_perezoso())
+        self.agregar_bicho(bicho4)
+
+        hab1.bicho = bicho1
+        hab2.bicho = bicho2
+        hab3.bicho = bicho3
+        hab4.bicho = bicho4
+
+        laberinto.agregar_habitacion(hab1)
+        laberinto.agregar_habitacion(hab2)
+        laberinto.agregar_habitacion(hab3)
+        laberinto.agregar_habitacion(hab4)
+
+        return laberinto
 
--- main.py
+++ main.py
@@ -1,5 +1,153 @@
 from creator import Creator, CreatorB
 from juegoLaberinto import Juego
+
+
+FM = Creator()
+juego = Juego()
+juego.laberinto = juego.crearLaberinto2HabFM(FM)
+hab1=juego.obtenerHabitacion(1)
+hab2=juego.obtenerHabitacion(2)
+print(hab1.num)
+print(hab2.num)
+
+#laberinto con paredes bomba
+FMb = CreatorB()
+juego.laberinto = juego.crearLaberinto2HabFM(FMb)
+hab1=juego.obtenerHabitacion(1)
+hab2=juego.obtenerHabitacion(2)
+print(hab1.norte.activa)
+print(hab2.sur.activa)
+
+
+# Crear laberinto de 4 habitaciones
+FM = Creator()
+juego.laberinto = juego.crearLaberinto4Hab(FM)
+
+
+
+# Mostrar el número de cada habitación
+for habitacion in juego.laberinto.habitaciones:
+    print(f"Habitación {habitacion.num}")
+    if hasattr(habitacion, 'bicho'):
+        bicho = habitacion.bicho
+        bicho = habitacion.bicho
+
+
+from creator import Creator, CreatorB
+from juegoLaberinto import Juego, Bomba
+
+
+#ejemplo de uso
+FM = Creator()
+juego = Juego()
+juego.laberinto = juego.crearLaberinto2HabFM(FM)
+hab1=juego.obtenerHabitacion(1)
+hab2=juego.obtenerHabitacion(2)
+print(hab1.num)
+print(hab2.num)
+
+
+
+
+#laberinto con paredes bomba
+FMb = CreatorB()
+juego.laberinto = juego.crearLaberinto2HabFM(FMb)
+hab1=juego.obtenerHabitacion(1)
+hab2=juego.obtenerHabitacion(2)
+print(hab1.norte.activa)
+print(hab2.sur.activa)
+
+# Crear laberinto de 4 habitaciones
+FM = Creator()
+juego.laberinto = juego.crearLaberinto4Hab(FM)
+
+# Mostrar el número de cada habitación
+for habitacion in juego.laberinto.habitaciones:
+    print(f"Habitación {habitacion.num}")
+    if hasattr(habitacion, 'bicho'):
+        bicho = habitacion.bicho
+
+
+from creator import Creator, CreatorB
+from juegoLaberinto import Juego, Bomba
+
+
+#ejemplo de uso
+FM = Creator()
+juego = Juego()
+juego.laberinto = juego.crearLaberinto2HabFM(FM)
+hab1=juego.obtenerHabitacion(1)
+hab2=juego.obtenerHabitacion(2)
+print(hab1.num)
+print(hab2.num)
+
+
+
+
+#laberinto con paredes bomba
+FMb = CreatorB()
+juego.laberinto = juego.crearLaberinto2HabFM(FMb)
+hab1=juego.obtenerHabitacion(1)
+hab2=juego.obtenerHabitacion(2)
+print(hab1.norte.activa)
+print(hab2.sur.activa)
+
+# Crear laberinto de 4 habitaciones
+FM = Creator()
+juego.laberinto = juego.crearLaberinto4Hab(FM)
+
+# Mostrar el número de cada habitación
+for habitacion in juego.laberinto.habitaciones:
+    print(f"Habitación {habitacion.num}")
+    if hasattr(habitacion, 'bicho'):
+        bicho = habitacion.bicho
+
+
+
+from creator import Creator, CreatorB
+from juegoLaberinto import Juego, Bomba
+
+
+
+
+#ejemplo de uso
+fm = Creator()
+juego = Juego()
+juego.laberinto = juego.crearLaberinto2HabFM(fm)
+hab1=juego.obtenerHabitacion(1)
+hab2=juego.obtenerHabitacion(2)
+print(hab1.num)
+print(hab2.num)
+
+
+
+
+#laberinto con paredes bomba
+FMb = CreatorB()
+juego.laberinto = juego.crearLaberinto2HabFM(FMb)
+hab1=juego.obtenerHabitacion(1)
+hab2=juego.obtenerHabitacion(2)
+print(hab1.norte.activa)
+print(hab2.sur.activa)
+
+
+
+
+# Crear laberinto de 4 habitaciones
+FM = Creator()
+juego.laberinto = juego.crearLaberinto4Hab(FM)
+
+# Mostrar el número de cada habitación
+for habitacion in juego.laberinto.habitaciones:
+    print(f"Habitación {habitacion.num}")
+    if hasattr(habitacion, 'bicho'):
+        bicho = habitacion.bicho
+
+
+from creator import Creator, CreatorB
+from juegoLaberinto import Juego, Bomba
+
+
 #ejemplo de uso
 fm = Creator()
 juego = Juego()
@@ -10,9 +158,71 @@ print(hab1.num)
 print(hab2.num)
 
 #laberinto con paredes bomba
-fmb = CreatorB()
-juego.laberinto = juego.crearLaberinto2HabFM(fmb)
+FMb = CreatorB()
+juego.laberinto = juego.crearLaberinto2HabFM(FMb)
 hab1=juego.obtenerHabitacion(1)
 hab2=juego.obtenerHabitacion(2)
 print(hab1.norte.activa)
 print(hab2.sur.activa)
+
+
+
+
+# Crear laberinto de 4 habitaciones
+FM = Creator()
+juego.laberinto = juego.crearLaberinto4Hab(FM)
+
+# Mostrar el número de cada habitación
+for habitacion in juego.laberinto.habitaciones:
+    print(f"Habitación {habitacion.num}")
+    if hasattr(habitacion, 'bicho'):
+        bicho = habitacion.bicho
+
+
+
+from creator import Creator, CreatorB
+from juegoLaberinto import Juego, Bomba
+
+
+
+#ejemplo de uso
+FM = Creator()
+juego = Juego()
+juego.laberinto = juego.crearLaberinto2HabFM(FM)
+hab1=juego.obtenerHabitacion(1)
+hab2=juego.obtenerHabitacion(2)
+print(hab1.num)
+print(hab2.num)
+
+#laberinto con paredes bomba
+FMb = CreatorB()
+juego.laberinto = juego.crearLaberinto2HabFM(FMb)
+hab1=juego.obtenerHabitacion(1)
+hab2=juego.obtenerHabitacion(2)
+print(hab1.norte.activa)
+print(hab2.sur.activa)
+
+
+
+
+# Crear laberinto de 4 habitaciones
+FM = Creator()
+juego.laberinto = juego.crearLaberinto4Hab(FM)
+
+# Mostrar el número de cada habitación
+for habitacion in juego.laberinto.habitaciones:
+    print(f"Habitación {habitacion.num}")
+    if hasattr(habitacion, 'bicho'):
+        bicho = habitacion.bicho
+        print(f"  Bicho: Vidas={bicho.vidas}, Poder={bicho.poder}, Posicion={bicho.posicion.num}")
+
+# Crear laberinto de 2 habitaciones con bombas (Habitación 1 al este, Habitación 2 al oeste)
+FM = Creator()
+juego2 = Juego()
+juego2.laberinto = juego2.crearLaberinto2HabBomba(FM)
+hab1 = juego2.obtenerHabitacion(1)
+hab2 = juego2.obtenerHabitacion(2)
+
+print("\nLaberinto de 2 habitaciones con bombas:")
+print(f"Habitación 1 tiene bomba al este: {hasattr(hab1, 'este') and hasattr(hab1.este, 'esBomba') and hab1.este.esBomba()}")
+print(f"Habitación 2 tiene bomba al oeste: {hasattr(hab2, 'oeste') and hasattr(hab2.oeste, 'esBomba') and hab2.oeste.esBomba()}")
